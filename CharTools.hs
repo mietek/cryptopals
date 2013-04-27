f8499451bf69ccc8c4281b4376855418b3588800
Miëtek Bak <mietek@bak.io>
2013-04-27 02:50:35 +0100

    Move code around

--- CharTools.hs
+++ CharTools.hs
@@ -1,6 +1,6 @@
 module CharTools where
 
-import Data.Bits (testBit, xor)
+import Data.Bits ((.|.), (.&.), shiftL, shiftR, testBit, xor)
 import Data.Char (chr, ord)
 
 
@@ -41,6 +41,20 @@ toHexDigit n
   | otherwise = error ("toHexDigit: invalid number " ++ show n)
 
 
+fromHexPair :: Char -> Char -> Char
+fromHexPair c1 c2 = chr n
+  where
+    n = (k1 `shiftL` 4) .|. k2
+    [k1, k2] = map fromHexDigit [c1, c2]
+
+toHexPair :: Char -> [Char]
+toHexPair c = map toHexDigit [k1, k2]
+  where
+    n = ord c
+    k1 = (n .&. 0xF0) `shiftR` 4
+    k2 = n .&. 0x0F
+
+
 isB64Digit :: Char -> Bool
 isB64Digit c =
     c >= 'A' && c <= 'Z' ||
@@ -70,6 +84,24 @@ toB64Digit n
   | n == 62 = '+'
   | n == 63 = '/'
   | otherwise = error ("toB64Digit: invalid number " ++ show n)
+
+
+fromB64Quad :: Char -> Char -> Char -> Char -> [Char]
+fromB64Quad c1 c2 c3 c4 = map chr [n1, n2, n3]
+  where
+    n1 = (k1 `shiftL` 2) .|. ((k2 .&. 0x30) `shiftR` 4)
+    n2 = ((k2 .&. 0x0F) `shiftL` 4) .|. ((k3 .&. 0x3C) `shiftR` 2)
+    n3 = ((k3 .&. 0x03) `shiftL` 6) .|. k4
+    [k1, k2, k3, k4] = map fromB64Digit [c1, c2, c3, c4]
+
+toB64Quad :: Char -> Char -> Char -> [Char]
+toB64Quad c1 c2 c3 = map toB64Digit [k1, k2, k3, k4]
+  where
+    k1 = (n1 .&. 0xFC) `shiftR` 2
+    k2 = ((n1 .&. 0x03) `shiftL` 4) .|. ((n2 .&. 0xF0) `shiftR` 4)
+    k3 = ((n2 .&. 0x0F) `shiftL` 2) .|. ((n3 .&. 0xC0) `shiftR` 6)
+    k4 = n3 .&. 0x3F
+    [n1, n2, n3] = map ord [c1, c2, c3]
 
 
 xorChar :: Char -> Char -> Char
--- StringTools.hs
+++ StringTools.hs
@@ -1,7 +1,5 @@
 module StringTools where
 
-import Data.Bits ((.|.), (.&.), shiftL, shiftR)
-import Data.Char (chr, ord)
 import Data.Function (on)
 import Data.List (sortBy, transpose)
 import Data.Maybe (catMaybes, listToMaybe)
@@ -10,37 +8,15 @@ import CharTools
 import Tools
 
 
-fromHexPair :: Char -> Char -> Char
-fromHexPair c1 c2 = chr n
-  where
-    n = (k1 `shiftL` 4) .|. k2
-    [k1, k2] = map fromHexDigit [c1, c2]
-
 fromHexString :: String -> String
 fromHexString [] = []
 fromHexString [c1] = error ("fromHexString: truncated digits " ++ show [c1])
 fromHexString (c1 : c2 : cs) = fromHexPair c1 c2 : fromHexString cs
 
-
-toHexPair :: Char -> String
-toHexPair c = map toHexDigit [k1, k2]
-  where
-    n = ord c
-    k1 = (n .&. 0xF0) `shiftR` 4
-    k2 = n .&. 0x0F
-
 toHexString :: String -> String
 toHexString [] = []
 toHexString (c : cs) = toHexPair c ++ toHexString cs
 
-
-fromB64Quad :: Char -> Char -> Char -> Char -> String
-fromB64Quad c1 c2 c3 c4 = map chr [n1, n2, n3]
-  where
-    n1 = (k1 `shiftL` 2) .|. ((k2 .&. 0x30) `shiftR` 4)
-    n2 = ((k2 .&. 0x0F) `shiftL` 4) .|. ((k3 .&. 0x3C) `shiftR` 2)
-    n3 = ((k3 .&. 0x03) `shiftL` 6) .|. k4
-    [k1, k2, k3, k4] = map fromB64Digit [c1, c2, c3, c4]
 
 fromB64String :: String -> String
 fromB64String [] = []
@@ -50,16 +26,6 @@ fromB64String [c1, c2, '=', '='] = take 1 (fromB64Quad c1 c2 '=' '=')
 fromB64String [c1, c2, c3] = take 2 (fromB64Quad c1 c2 c3 '=')
 fromB64String [c1, c2, c3, '='] = take 2 (fromB64Quad c1 c2 c3 '=')
 fromB64String (c1 : c2 : c3 : c4 : cs) = fromB64Quad c1 c2 c3 c4 ++ fromB64String cs
-
-
-toB64Quad :: Char -> Char -> Char -> String
-toB64Quad c1 c2 c3 = map toB64Digit [k1, k2, k3, k4]
-  where
-    k1 = (n1 .&. 0xFC) `shiftR` 2
-    k2 = ((n1 .&. 0x03) `shiftL` 4) .|. ((n2 .&. 0xF0) `shiftR` 4)
-    k3 = ((n2 .&. 0x0F) `shiftL` 2) .|. ((n3 .&. 0xC0) `shiftR` 6)
-    k4 = n3 .&. 0x3F
-    [n1, n2, n3] = map ord [c1, c2, c3]
 
 toB64String :: String -> String
 toB64String [] = []
