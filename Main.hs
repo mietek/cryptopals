25bdd9f79a0f7dc0a004ee10e9f4ff0405e5a5b1
Miëtek Bak <mietek@bak.io>
2013-04-29 03:27:10 +0100

    Replace fmap with <$>

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TupleSections #-}
 
 module Main where
 
+import Control.Applicative ((<$>))
 import Crypto.Cipher.AES (initKey, decryptECB)
 import qualified Data.ByteString.Char8 as BS
 import Data.Maybe (catMaybes, fromJust)
@@ -36,7 +36,7 @@ case_3 = fromJust (crack1Xor s) @?= result
 
 case_4 :: Assertion
 case_4 = do
-    ss <- fmap (map fromHex . BS.lines) (BS.readFile "case_4.txt")
+    ss <- map fromHex . BS.lines <$> BS.readFile "case_4.txt"
     head (orderDescendingOn (scorePhrase . fst) (catMaybes (map crack1Xor ss))) @?= result
   where
     result = ("Now that the party is jumping\n", "5")
@@ -50,7 +50,7 @@ case_5 = toHex (key `xor` s) @?= result
 
 case_6 :: Assertion
 case_6 = do
-    s <- fmap (fromB64 . BS.concat . BS.lines) (BS.readFile "case_6.txt")
+    s <- fromB64 . BS.concat . BS.lines <$> BS.readFile "case_6.txt"
     result <- BS.readFile "result_6.txt"
     fromJust (crackXor s) @?= (result, resultKey)
   where
@@ -58,7 +58,7 @@ case_6 = do
 
 case_7 :: Assertion
 case_7 = do
-    s <- fmap (fromB64 . BS.concat . BS.lines) (BS.readFile "case_7.txt")
+    s <- fromB64 . BS.concat . BS.lines <$> BS.readFile "case_7.txt"
     result <- BS.readFile "result_7.txt"
     decryptECB key s @?= result
   where
@@ -66,7 +66,7 @@ case_7 = do
 
 case_8 :: Assertion
 case_8 = do
-    ss <- fmap (map fromHex . BS.lines) (BS.readFile "case_8.txt")
+    ss <- map fromHex . BS.lines <$> BS.readFile "case_8.txt"
     head (filter detectECB ss) @?= result
   where
     result = fromHex "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
